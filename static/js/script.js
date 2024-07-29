1c1c4742357cfa62a3049a38072969d291e8975a
daedalus <0xda3d4lus@proton.me>
2024-07-29 17:17:32 +0000

    initialcommit

---
+++ static/js/script.js
@@ -0,0 +1,12 @@
+var i = 0;
+var txt = 'Hello, friend..'; /* The text */
+var speed = 350; /* The speed/duration of the effect in milliseconds */
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("demo").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
+typeWriter()
